0547ed73736e8faccfdabed120bb4bc02ab20da7
Roshan17431 <roshanmathew414@gmail.com>
2025-12-10 07:23:57 +0530

    Update 15

---
+++ src/main/java/com/example/MovieHub/service/AuthService.java
@@ -0,0 +1,54 @@
+package com.example.MovieHub.service;
+
+import com.example.MovieHub.dto.auth.AuthResponse;
+import com.example.MovieHub.dto.auth.LoginRequest;
+import com.example.MovieHub.dto.auth.RegisterRequest;
+import com.example.MovieHub.entity.User;
+import com.example.MovieHub.entity.UserRole;
+import com.example.MovieHub.repo.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        if(userRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRoles(Set.of(UserRole.ROLE_USER));
+        userRepository.save(user);
+        String token = jwtService.generateToken(user);
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+        authenticationManager.authenticate(authenticationToken);
+        User user = userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow(()-> new IllegalArgumentException("User not found"));
+        String token = jwtService.generateToken(user);
+        return new AuthResponse(token);
+    }
+}
