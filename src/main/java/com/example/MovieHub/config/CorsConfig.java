1614ffeb53cbbe95132cd19faaaee2aa38db19e7
Roshan17431 <roshanmathew414@gmail.com>
2025-12-13 15:22:20 +0530

    Update 26

---
+++ src/main/java/com/example/MovieHub/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.MovieHub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://movie-hub-pnva.vercel.app",
+                                "http://localhost:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
